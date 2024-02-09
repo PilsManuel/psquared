ed9b10e174e08808cb9c9a7dd5191cd0663e087b
Martin Pammesberger <pammesbergerma@gmail.com>
2024-02-09 14:54:34 +0100

    add footer

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@ import Subscription from "./src/components/Subscription.vue";
 import HeroImage from "./src/components/Hero.vue";
 import Navbar from "./src/components/Navbar.vue";
 import About from "./src/components/About.vue";
+import PsquaredFooter from "./src/components/Footer.vue";
 
 export default {
   components: {
@@ -10,6 +11,7 @@ export default {
     HeroImage,
     Navbar,
     About,
+    PsquaredFooter,
   },
   data() {
     return {
@@ -18,13 +20,19 @@ export default {
   },
 };
 </script>
-
 <template>
   <div ref="home" class="body">
     <Navbar />
     <HeroImage alt="Hero Image" />
-    <About />
-    <Subscription />
+    <div class="tiles">
+      <div class="tile">
+        <About />
+      </div>
+      <div class="tile">
+        <Subscription />
+      </div>
+    </div>
+    <PsquaredFooter />
   </div>
 </template>
 
@@ -34,5 +42,36 @@ ul {
   margin: 0;
   padding: 0;
   background-color: #222;
+}
+/* Hide scrollbar for Chrome, Safari, and Opera */
+::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge, and Firefox */
+* {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+.tiles {
+  display: flex;
+  flex-direction: column;
+  padding: 0 24px 0 24px;
+  display: flex;
+}
+.tile {
+  background-color: #999;
+  color: #333;
+  margin: 50px auto 50px; /* Added margin-bottom */
+  text-align: center;
+  width: 1040px;
+  max-width: 100%;
+  /* max-height: 40vh; */
+  overflow: auto;
+  box-sizing: border-box;
+  border-radius: 15px;
+  padding: 50px 30px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  font-family: "Poppins", sans-serif; /* Changed font */
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,85 @@
+<template>
+  <footer class="footer">
+    <hr />
+    <div class="footer-container">
+      <div class="footer-section">
+        <h3>Frequent Updates</h3>
+        <a href="https://twitter.com/Graffino" target="_blank">Twitter</a>
+        <a href="https://github.com/Graffino" target="_blank">Github</a>
+        <a href="https://linkedin.com/company/Graffino" target="_blank"
+          >LinkedIn</a
+        >
+        <a href="https://medium.com/@Graffino" target="_blank">Medium</a>
+        <a href="https://instagram.com/GraffinoWeb" target="_blank"
+          >Instagram</a
+        >
+      </div>
+      <!-- Additional sections can be added here -->
+      <div class="footer-section footer-right">
+        <h3 class="privacy-section">© 2024 psquared</h3>
+        <a href="/privacy-policy">Privacy Policy</a>
+        <a href="/terms-of-service">Terms of Service</a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+.footer {
+  background-color: #222;
+  color: #fff;
+  font-family: "Poppins", sans-serif;
+  align-items: end;
+}
+
+.footer-container {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 24px 24px 24px;
+}
+
+.footer-section {
+  display: flex;
+  flex-direction: column;
+}
+
+.footer-section.footer-right {
+  align-items: flex-end;
+}
+
+.footer-section h3 {
+  color: #fff;
+  margin-bottom: 10px;
+  font-size: 15px;
+}
+
+.footer-section a {
+  color: #fff;
+  margin-bottom: 5px;
+  transition: color 0.3s ease;
+  font-size: 12px;
+}
+
+.footer-section a:hover {
+  color: #ffd700;
+}
+
+.footer-copy {
+  text-align: center;
+  border-top: 1px solid #333;
+  padding-top: 20px;
+}
+
+a {
+  color: #fff;
+  text-decoration: none;
+  font-size: 12px;
+  transition: color 0.3s ease;
+}
+</style>
